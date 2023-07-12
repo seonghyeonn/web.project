a1c116ec7b4be17d2d1fdcdf4c224c41654aabca
seonghyeonn <shyexnnn@gmail.com>
2023-07-12 16:07:13 +0900

    glaemfdjdjdjdjdj

--- mainpage/mainpage.js
+++ mainpage/mainpage.js
@@ -1,12 +1,12 @@
 function mainPage() {
-    window.location.href = "https://seonghyeonn.github.io/file:///Users/bagseonghyeon/Desktop/project.web/mainpage/mainpage.html";
+    window.location.href = "file:///Users/bagseonghyeon/Desktop/project.web/mainpage/mainpage.html";
 }
 function projectPage() {
-    window.location.href = "https://seonghyeonn.github.io/file:///Users/bagseonghyeon/Desktop/project.web/project/project.html";
+    window.location.href = "file:///Users/bagseonghyeon/Desktop/project.web/project/project.html";
 }
 function studyPage() {
-    window.location.href = "https://seonghyeonn.github.io/file:///Users/bagseonghyeon/Desktop/project.web/study/study.html";
+    window.location.href = "file:///Users/bagseonghyeon/Desktop/project.web/study/study.html";
 }
 function contactPage() {
-    window.location.href = "https://seonghyeonn.github.io/file:///Users/bagseonghyeon/Desktop/project.web/contact/contact.html";
+    window.location.href = "file:///Users/bagseonghyeon/Desktop/project.web/contact/contact.html";
 }
